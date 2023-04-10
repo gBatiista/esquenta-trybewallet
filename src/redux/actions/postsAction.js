1f4adeb27248c61d9cce5fb4cc1ed9c4eb4e7e62
caren <carenoliveirasilvapontes@gmail.com>
2023-04-10 17:54:47 -0300

    feat:post action finalizado

--- src/redux/actions/postsAction.js
+++ src/redux/actions/postsAction.js
@@ -1,12 +1,9 @@
-import { FAILED_REQUEST, SUCCEEDED_REQUEST } from '../types/postsType';
+import fetchPosts from '../../utils/fetchPosts';
+import { FAILED_REQUEST, GET_POSTS, SUCCEEDED_REQUEST } from '../types/postsType';
 
-// export const getPosts = () => {
-//   try {
-
-//   } catch (error) {
-
-//   }
-// };
+export const getPosts = () => ({
+  type: GET_POSTS,
+});
 export const succedRequest = (payload) => ({
   type: SUCCEEDED_REQUEST,
   payload,
@@ -15,3 +12,14 @@ export const failedRequest = (error) => ({
   type: FAILED_REQUEST,
   payload: error,
 });
+
+export const fetchApiThunk = () => async (dispatch) => {
+  try {
+    dispatch(getPosts());// mudar isloading para true
+    const posts = await fetchPosts();
+    console.log(posts.filter((element) => element.userId !== 2));
+    dispatch(succedRequest(posts.filter((element) => element.userId === 2)));
+  } catch (error) {
+    dispatch(failedRequest(error.message));
+  }
+};
