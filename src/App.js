ae5d961081b492e1cd1ce31ee6d79c73e3c055b1
caren <carenoliveirasilvapontes@gmail.com>
2023-04-10 08:07:14 -0300

    feat:routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
-  return (
-    <div className="App">
-      <div>Esquenta</div>
-    </div>
-  );
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+class App extends React.Component() {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/posts" component={ Posts } />
+      </Switch>
+
+    );
+  }
 }
 
 export default App;
