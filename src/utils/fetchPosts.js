448b0751a75919ec7e0e5859da77acc9411be367
Gabriel Batista <g.batiista@gmail.com>
2023-04-10 23:32:30 -0300

    feat: create utils/fetchPosts

---
+++ src/utils/fetchPosts.js
@@ -0,0 +1,6 @@
+const fetchPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  return data;
+};
+export default fetchPosts;
